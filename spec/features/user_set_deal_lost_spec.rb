68faee20f9ceda88c3d9a3278116d85951d2b0c3
Marcelo Toledo <marcelotoledo5000@gmail.com>
2018-04-18 12:44:29 -0300

    Set status to won or lost

---
+++ spec/features/user_set_deal_lost_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User set a deal as lost' do
+  scenario 'successfully' do
+    deal = create(:deal)
+    user = create(:user)
+    login_as user
+
+    visit root_path
+    click_on 'Lost'
+
+    deal.reload
+    expect(page).to have_content('Your deal was Lost!')
+    expect(page).to have_content('Current Deals: US$ 2,5 B')
+    expect(page).to have_css('td', text: 'Acme')
+    expect(page).to have_css('td', text: 'Multi Billion Dollars Sale')
+    expect(page).not_to have_css('td', text: 'Pending')
+    expect(page).to have_css('td', text: 'Lost')
+    expect(page).to have_css('td', text: '03/03/18')
+    expect(page).to have_css('td', text: 'US$ 2,5 B')
+  end
+end

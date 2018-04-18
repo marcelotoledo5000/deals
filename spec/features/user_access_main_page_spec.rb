a12e2efebb77129e56dcdb3ea315b3a1be871cb5
Marcelo Toledo <marcelotoledo5000@gmail.com>
2018-04-18 04:19:49 -0300

    User add deal

--- spec/features/user_access_main_page_spec.rb
+++ spec/features/user_access_main_page_spec.rb
@@ -34,7 +34,7 @@ feature 'User access main page' do
     create(:deal)
     deal2 = create(:deal, customer: 'W&B',
                           description: 'Multi Million Dollars Sale',
-                          status: 0, closing_date_probability: 'In 25 days 70%',
+                          status: 1, closing_date_probability: 'In 25 days 70%',
                           value: 'US$ 2,5 M')
     deal3 = create(:deal, customer: 'Brand',
                           description: 'Multi Thousand Dollars Sale',
@@ -52,19 +52,19 @@ feature 'User access main page' do
     expect(page).to have_css('th', text: 'Actions')
     expect(page).to have_css('td', text: 'Acme')
     expect(page).to have_css('td', text: 'Multi Billion Dollars Sale')
-    expect(page).to have_css('td', text: 'Won')
+    expect(page).to have_css('td', text: 'Pending')
     expect(page).to have_css('td', text: '03/03/18')
     expect(page).to have_css('td', text: 'US$ 2,5 B')
     expect(page).to have_css('td', text: 'Edit | Destroy | Won | Lost')
     expect(page).to have_css('td', text: deal2.customer)
     expect(page).to have_css('td', text: deal2.description)
-    expect(page).to have_css('td', text: deal2.status)
+    expect(page).to have_css('td', text: 'Won')
     expect(page).to have_css('td', text: deal2.closing_date_probability)
     expect(page).to have_css('td', text: deal2.value)
     expect(page).to have_css('td', text: 'Edit | Destroy | Won | Lost')
     expect(page).to have_css('td', text: deal3.customer)
     expect(page).to have_css('td', text: deal3.description)
-    expect(page).to have_css('td', text: deal3.status)
+    expect(page).to have_css('td', text: deal3.status.camelcase)
     expect(page).to have_css('td', text: deal3.closing_date_probability)
     expect(page).to have_css('td', text: deal3.value)
     expect(page).to have_css('td', text: 'Edit | Destroy | Won | Lost')

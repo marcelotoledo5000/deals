466c101097000bd71b8511e955020f7935162706
Marcelo Toledo <marcelotoledo5000@gmail.com>
2018-04-18 05:38:00 -0300

    User edit a deal

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -4,18 +4,38 @@ class DealsController < ApplicationController
   def create
     @deal = Deal.new deal_params
     @deal.closing_date_probability = default_time
+
     if @deal.save
       flash[:success] = 'Your deal was successfully sent!'
     else
       flash[:alert] = 'You need to fill all fields!'
     end
+
     redirect_to root_path
+  end
+
+  def edit
+    @deal = Deal.find_by(id: params[:id])
   end
 
   def index
     @deal = Deal.new
     @last_deal = Deal.last
     @deals = Deal.all
+  end
+
+  def update
+    deal = Deal.find_by(id: params[:id])
+
+    if deal.update(deal_params)
+      flash[:success] = 'Your deal successfully updated!'
+      redirect_to root_path
+    else
+      @deal = deal
+      flash[:alert] = 'You need to fill all fields!'
+
+      render :edit
+    end
   end
 
   private

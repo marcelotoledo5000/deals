69b4c49e291c6149aee36939e2be41cf2bf0c18a
Marcelo Toledo <marcelotoledo5000@gmail.com>
2019-05-27 22:07:21 -0300

    Added models tests

--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Deal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:customer) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:closing_date_probability) }
+  it { should validate_presence_of(:value) }
+  it { should define_enum_for(:status).with_values(%i[pending won lost]) }
 end

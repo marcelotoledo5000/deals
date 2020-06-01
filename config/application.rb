8bd1d3438dc275ebdaaa066a62e5886489423752
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-05-31 22:35:43 -0300

    Clean up

--- config/application.rb
+++ config/application.rb
@@ -10,24 +10,12 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require 'sprockets/railtie'
-# require 'rails/test_unit/railtie'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Deals
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
   end
 end

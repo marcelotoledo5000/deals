3ab350fc7326146ddf73a859708a423f2a679b3e
Marcelo Toledo <marcelotoledo5000@gmail.com>
2021-11-14 21:45:10 -0300

    Bump a lot gems

--- Gemfile
+++ Gemfile
@@ -5,41 +5,42 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.2'
 
-gem 'bootsnap', '~> 1.4', require: false
-gem 'bootstrap', '~> 4.3'
-gem 'devise', '~> 4.7'
-gem 'dotenv-rails', '~> 2.7'
-gem 'faraday', '~> 1.0.0.pre.rc1'
-gem 'jbuilder', '~> 2.9'
+gem 'bootsnap', '>= 1.4.4', require: false
+gem 'bootstrap'
+gem 'devise'
+gem 'dotenv-rails'
+gem 'faraday'
+gem 'jbuilder'
 gem 'jquery-rails', '~> 4.3'
 gem 'pg', '~> 1.2'
 gem 'puma', '~> 4.3'
-gem 'rails', '~> 6.0'
+gem 'rails', '~> 6.1.4'
+gem 'sass-rails', '>= 6'
 gem 'turbolinks', '~> 5.2'
-gem 'webpacker', '~> 4.0'
+gem 'webpacker', '~> 5.0'
 
 group :development, :test do
-  gem 'capybara', '~> 3.29'
+  gem 'capybara'
   gem 'codecov', require: false
-  gem 'database_cleaner', '~> 1.8'
-  gem 'factory_bot_rails', '~> 5.2'
-  gem 'faker', '~> 2.11'
-  gem 'pry-byebug', '~> 3.8'
-  gem 'rails_best_practices', '~> 1.19'
-  gem 'rspec-rails', '~> 4.0.0'
+  gem 'database_cleaner'
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'pry-byebug'
+  gem 'rspec-rails'
   gem 'rubocop'
   gem 'rubocop-performance'
   gem 'rubocop-rails'
   gem 'rubocop-rspec'
   gem 'rubycritic'
-  gem 'shoulda-matchers', '~> 4.3'
+  gem 'shoulda-matchers'
   gem 'simplecov'
 end
 
 group :development do
   gem 'brakeman'
-  gem 'listen', '~> 3.2'
+  gem 'listen'
+  # Can be configured to work on production as well see: https://github.com/MiniProfiler/rack-mini-profiler/blob/master/README.md
+  gem 'rack-mini-profiler'
   gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0'
-  gem 'web-console', '~> 4.0'
+  gem 'web-console'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 2018_04_23_015145) do
     t.integer "status", default: 0
     t.string "closing_date_probability"
     t.string "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_deals_on_user_id"
   end
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 2018_04_23_015145) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
